1031e40a2cf9ac9d223049c01f6de53045882685
Jugal Mehra <jugal.mehra@indianic.com>
2023-05-30 15:46:08 +0530

    change data type in front and update swagger

--- app/modules/Courses/Schema.js
+++ app/modules/Courses/Schema.js
@@ -11,7 +11,7 @@ const courseSchema = new Schema(
     category: {
       type: String,
       require: true,
-      enum: ["testbank", "writeAndImprove"],
+      enum: ["testbank", "writeAndImprove","ieltsebook","praxis","printpractice"],
       default: "testbank",
     },
     moduleType:{
