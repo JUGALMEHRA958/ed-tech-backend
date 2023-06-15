0f3544e138fec0109c2ce4b06d141b9e6f193faa
Jugal Mehra <jugal.mehra@indianic.com>
2023-06-15 18:48:47 +0530

    stripe register user

--- app/modules/Students/Controller.js
+++ app/modules/Students/Controller.js
@@ -139,7 +139,7 @@ class StudentsController extends Controller {
           let stripeObj = await new StripeService().createStripeUser(newUserId.email);
           // console.log(stripeObj,"stripeObj");
           if(stripeObj && stripeObj.status==1 && stripeObj.data.id){
-            await Students.findOneAndUpdate({_id:newUserId._id} , {stripeCustomerId:stripeObj.data.id })
+            newUserId = await Students.findOneAndUpdate({_id:newUserId._id} , {stripeCustomerId:stripeObj.data.id })
           }
           let { token, refreshToken } =
             await new Globals().getTokenWithRefreshToken({ id: newUserId._id });
