ae3c1a1f828ac8b834e216c49a576bcf4fea77d8
unknown <jugal.mehra@indianic.com>
2023-06-26 23:30:29 +0530

    update the stripe

--- app/modules/Admin/Routes.js
+++ app/modules/Admin/Routes.js
@@ -106,9 +106,9 @@ module.exports = (app, express) => {
         return adminObj.updateGroupById();
     });
 
-    router.delete('/admin/deleteGroupById', Globals.isAdminAuthorised(),Validators.getGroupByIdValidator(), Validators.validate, (req, res, next) => {
+    router.delete('/admin/deleteDiscountGroupById', Globals.isAdminAuthorised(),Validators.getGroupByIdValidator(), Validators.validate, (req, res, next) => {
         const adminObj = (new AdminController()).boot(req, res);
-        return adminObj.deleteGroupById();
+        return adminObj.deleteDiscountGroupById();
     });
 
 
