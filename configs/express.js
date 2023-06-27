cde1d619638be921ed3b2ff97cf60ed2f1d1ee18
Jugal Mehra <jugal.mehra@indianic.com>
2023-06-27 12:26:12 +0530

    updatebulk

--- configs/express.js
+++ configs/express.js
@@ -61,7 +61,7 @@ module.exports = function () {
   // =======   Settings for CORS
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", '*');
     next();
   });
 
